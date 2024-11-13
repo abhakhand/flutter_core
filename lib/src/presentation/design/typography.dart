3d8f0c090473869677043053a1f2cd935c44780a
abhakhand <reachbhakhand@gmail.com>
2024-11-13 14:20:04 +0530

    feat: added font variations

--- lib/src/presentation/design/typography.dart
+++ lib/src/presentation/design/typography.dart
@@ -87,34 +87,49 @@ abstract interface class AppTypography {
   /// very fine text or microcopy.
   ///
   /// Returns a `TextStyle` with a font size of 10.0 pixels.
-  static TextStyle mini = TextStyle(fontSize: miniFontSize);
+  static TextStyle mini = TextStyle(
+    fontSize: miniFontSize,
+    fontVariations: const [FontVariation.weight(300)],
+  );
 
   /// A text style for captions or small labels.
   ///
   /// Returns a `TextStyle` with a font size of 12.0 pixels.
-  static TextStyle caption = TextStyle(fontSize: captionFontSize);
+  static TextStyle caption = TextStyle(
+    fontSize: captionFontSize,
+    fontVariations: const [FontVariation.weight(300)],
+  );
 
   /// A text style for small body text.
   ///
   /// Returns a `TextStyle` with a font size of 13.0 pixels.
-  static TextStyle bodySmall = TextStyle(fontSize: bodySmallFontSize);
+  static TextStyle bodySmall = TextStyle(
+    fontSize: bodySmallFontSize,
+    fontVariations: const [FontVariation.weight(400)],
+  );
 
   /// A text style for regular body text.
   ///
   /// Returns a `TextStyle` with a font size of 14.0 pixels.
-  static TextStyle bodyRegular = TextStyle(fontSize: bodyRegularFontSize);
+  static TextStyle bodyRegular = TextStyle(
+    fontSize: bodyRegularFontSize,
+    fontVariations: const [FontVariation.weight(400)],
+  );
 
   /// A text style for large body text, typically used for prominent body copy.
   ///
   /// Returns a `TextStyle` with a font size of 16.0 pixels.
-  static TextStyle bodyLarge = TextStyle(fontSize: bodyLargeFontSize);
+  static TextStyle bodyLarge = TextStyle(
+    fontSize: bodyLargeFontSize,
+    fontVariations: const [FontVariation.weight(400)],
+  );
 
   /// A text style for small titles or headings.
   ///
   /// Returns a `TextStyle` with a font size of 20.0 pixels and font weight 500.
   static TextStyle titleSmall = TextStyle(
     fontSize: titleSmallFontSize,
-    fontWeight: FontWeight.w500,
+    fontVariations: const [FontVariation.weight(500)],
   );
 
   /// A text style for regular titles or headings.
@@ -122,7 +137,7 @@ abstract interface class AppTypography {
   /// Returns a `TextStyle` with a font size of 24.0 pixels and font weight 500.
   static TextStyle titleRegular = TextStyle(
     fontSize: titleRegularFontSize,
-    fontWeight: FontWeight.w500,
+    fontVariations: const [FontVariation.weight(500)],
   );
 
   /// A text style for medium-sized titles or headings.
@@ -130,7 +145,7 @@ abstract interface class AppTypography {
   /// Returns a `TextStyle` with a font size of 28.0 pixels and font weight 500.
   static TextStyle titleMedium = TextStyle(
     fontSize: titleMediumFontSize,
-    fontWeight: FontWeight.w500,
+    fontVariations: const [FontVariation.weight(500)],
   );
 
   /// A text style for large titles or headings.
@@ -138,7 +153,7 @@ abstract interface class AppTypography {
   /// Returns a `TextStyle` with a font size of 32.0 pixels and font weight 500.
   static TextStyle titleLarge = TextStyle(
     fontSize: titleRegularFontSize,
-    fontWeight: FontWeight.w500,
+    fontVariations: const [FontVariation.weight(500)],
   );
 
   /// A text style for extra-large titles or headings.
@@ -146,29 +161,6 @@ abstract interface class AppTypography {
   /// Returns a `TextStyle` with a font size of 36.0 pixels and font weight 500.
   static TextStyle titleExtraLarge = TextStyle(
     fontSize: titleExtraLargeFontSize,
-    fontWeight: FontWeight.w500,
+    fontVariations: const [FontVariation.weight(500)],
   );
 }
-
-// /// A custom implementation of the `ITypography` class that
-// /// overrides certain properties.
-// interface class AppTypography extends ITypography {
-//   AppTypography._(); // Private constructor to prevent instantiation.
-
-//   static final AppTypography _instance = AppTypography._();
-
-//   // Public static instance getter.
-//   static AppTypography get instance => _instance;
-// }
-
-// class SomeWidget extends StatelessWidget {
-//   const SomeWidget({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Text(
-//       'Test',
-//       style: AppTypography.instance.bodyLarge,
-//     );
-//   }
-// }
